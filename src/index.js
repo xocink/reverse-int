b46aff32cd98c0dfd36168d94809db9595b87803
xocink <57539437+xocink@users.noreply.github.com>
2021-02-02 21:09:12 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    if (n < 0) {
+        let value = String(n).split('').reverse();
+        value.pop();
+        value = value.join('');
+        return +value;
+    } else {
+        let value = String(n).split('').reverse().join('');
+        return +value;
+    }
 }
